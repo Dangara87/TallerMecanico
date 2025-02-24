5aff895912a16286e9c4c596e23b2e0b2925146f
José Daniel García Salamanca <danigarcisala@gmail.com>
2025-02-24 09:47:18 +0100

    Clientes terminado y comenzando Revisiones y Vehiculos

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/Revisiones.java
@@ -0,0 +1,62 @@
+/*package org.iesalandalus.programacion.tallermecanico.modelo.negocio;
+
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Cliente;
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Revision;
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Vehiculo;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Revisiones {
+
+    private List<Revision> revision;
+
+    public Revisiones() {
+
+    }
+
+    public Revision[] get() {
+
+    }
+
+    public Revision[] get(Cliente cliente) {
+
+    }
+
+    public Revision[] get(Vehiculo vehiculo) {
+
+    }
+
+    public void insertar(Revision revision) {
+
+    }
+
+    private void comprobarRevision(Cliente cliente, Vehiculo vehiculo, LocalDate fechaRevision) {
+
+    }
+
+    public Revision anadirHoras(Revision revision, int horas) {
+
+    }
+
+    private Revision getRevision(Revision revision) {
+
+    }
+
+    public Revision anadirPrecioMaterial(Revision revision, float precioMaterial) {
+
+    }
+
+    public Revision cerrar(Revision revision, LocalDate fechaFin) {
+
+    }
+
+    public Revision buscar(Revision revision) {
+
+    }
+
+    public void borrar(Revision revision) {
+
+    }
+}
+*/
---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/Vehiculos.java
@@ -0,0 +1,32 @@
+/*package org.iesalandalus.programacion.tallermecanico.modelo.negocio;
+
+import org.iesalandalus.programacion.tallermecanico.modelo.dominio.Vehiculo;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Vehiculos {
+
+    private List<Vehiculo> vehiculo;
+
+    public Vehiculos() {
+
+    }
+
+    public Vehiculo[] get() {
+
+    }
+
+    public void insertar(Vehiculo vehiculo) {
+        Objects.requireNonNull(vehiculo, "No se puede");
+    }
+
+    public Vehiculo buscar(Vehiculo vehiculo) {
+
+    }
+
+    public void borrar(Vehiculo vehiculo) {
+
+    }
+}
+*/
